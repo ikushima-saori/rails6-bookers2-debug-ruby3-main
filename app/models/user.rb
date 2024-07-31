a9c5ea9b5e3dccabe56bf8a2de2203c1ba2efd94
ikushima-saori <172108271+ikushima-saori@users.noreply.github.com>
2024-07-31 09:34:28 +0000

    [Add]各モデルに検索方法の分岐

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,17 +19,28 @@ class User < ApplicationRecord
   def follow(user)
     follower.create(followed_id: user.id)
   end
-
   def unfollow(user)
     follower.find_by(followed_id: user.id).destroy
   end
-
   def following?(user)
     follower_user.include?(user)
   end
-
   def get_profile_image
     (profile_image.attached?) ? profile_image : 'no_image.jpg'
+  end
+
+  def self.looks(search, word)
+    if search == "perfect_match"
+      @user = User.where("name LIKE?", "#{word}")
+    elsif search == "forward_match"
+      @user = User.where("name LIKE?","#{word}%")
+    elsif search == "backward_match"
+      @user = User.where("name LIKE?","%#{word}")
+    elsif search == "partial_match"
+      @user = User.where("name LIKE?","%#{word}%")
+    else
+      @user = User.all
+    end
   end
 
 end
