463553d82ae6630cd25d225c17c188e0ef210fe9
ikushima-saori <172108271+ikushima-saori@users.noreply.github.com>
2024-07-31 11:22:24 +0000

    [interruption]検索中断

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,6 +1,9 @@
 class SearchesController < ApplicationController
   def search
     @model = params[:model]
+    @search = params[:search]
+    @word = params[:word]
+
 
     if @model == "User"
       @users = User.looks(params[:search], params[:word])
