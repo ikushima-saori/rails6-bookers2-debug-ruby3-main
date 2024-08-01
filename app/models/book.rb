2c4f6235bfdd668d0e3b58eb411297d139d8a836
ikushima-saori <172108271+ikushima-saori@users.noreply.github.com>
2024-08-01 06:37:04 +0000

    レイアウト調整

--- app/models/book.rb
+++ app/models/book.rb
@@ -7,13 +7,13 @@ class Book < ApplicationRecord
 
   def self.search_for(content, method)
     if method == 'perfect'
-      Book.where(title: content)
+      Book.where("title LIKE ?", "#{content}")
     elsif method == 'forward'
-      Book.where('title LIKE ?', content + '%')
+      Book.where('title LIKE ?', "#{content}%")
     elsif method == 'backward'
-      Book.where('title LIKE ?', '%' + content)
+      Book.where('title LIKE ?', "%#{content}")
     else
-      Book.where('title LIKE ?', '%' + content + '%')
+      Book.where('title LIKE ?', "%#{content}%")
     end
   end
 
